41d9e024dc0b7ead2cddb41755ec6b6a09a65d3d
ArchchanaKrishnathas <archchana2504@gmail.com>
2025-08-08 13:54:59 +0530

    Add examples for for & while loops in Python
    forLoop.py and whileloops.py, containing various example usages of for and while loops in Python, including range iterations, string iteration, break/continue statements, and nested loops.

---
+++ forLoop.py
@@ -0,0 +1,29 @@
+# for x in range(5):
+#     print(x)
+
+# for x in range(5,10):
+#     print(x)
+
+# for x in range(5,10,2):
+#     print(x)
+
+# for x in range(10,1,-1):
+#     print(x)
+
+# for x in True:
+#     print(x)    # wrong
+
+# for x in "Archchana":
+#     print(x)   
+
+
+# for x in range(10):
+#     if x==5:
+#         break
+#     print(x)   
+
+# for x in range(10):
+#     if x==5:
+#         continue
+#     print(x)   
+
---
+++ whileloops.py
@@ -0,0 +1,69 @@
+x=1
+while x<= 10:
+    if x%2 == 1:
+        print(x)
+    x+=1
+
+x=1
+while x<= 10:
+    if x%2 == 0:
+        print(x)
+    x+=1
+
+
+x = 1
+while x <= 5:
+    print(x * (x + 1) // 2)
+    x += 1
+
+   
+x=1
+while x<= 5:
+    print(x*x)
+    x+=1
+
+x=1
+while x<= 10:
+    if x% 2 == 1:
+        print(x, " odd")
+    else:
+        print(x, " even")
+    x+=1
+
+
+x=1
+while x<= 10:
+    if x% 2 == 1:
+        print(x, end=" ")
+    else:
+        print(x, end=" ")
+    x+=1
+
+x=1
+while x<= 10:
+    if x% 2 == 1:
+        print(x,'*' )
+    else:
+        print(x, '**')
+    x+=1
+
+
+x=1
+while x<=5:
+    y=1
+    while y<=5:
+        print(y,end='')
+        y+=1
+    print(x)
+    x+=1    
+
+x=1
+while x<=5:
+    y=1
+    while y<=x:
+        print(y,end='')
+        y+=1
+    print(x)
+    x+=1    
+
+    
