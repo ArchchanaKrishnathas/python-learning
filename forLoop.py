c218fd4d05cdfa9f3331769b262587ae613c1081
ArchchanaKrishnathas <archchana2504@gmail.com>
2025-08-11 05:28:48 +0530

    Add for loop examples and patterns
    Added multiple commented for loop examples demonstrating different patterns, including printing odd/even numbers, mathematical sequences, nested loops, and conditional printing. The final active code prints a right-angled triangle pattern using nested loops.

--- forLoop.py
+++ forLoop.py
@@ -27,3 +27,69 @@
 #         continue
 #     print(x)   
 
+
+
+# for x in range (10):
+#     if x % 2== 1:
+#         print(x, end=" ")
+
+# for x in range(1, 11):
+#     if x % 2 == 0:
+#         print(x, end=" ")
+
+# for x in range(1, 6):
+#     print( x * (x+1) // 2 , end=" ")
+
+# for x in range (1, 6):
+#     print(x*x, end=" ")
+
+# for x in range(1, 6):
+#     if x % 2 == 1:
+#         print(x, end=" ")
+#     else if x % 2 == 1:
+#         print('*', end=" ")
+
+# for x in range (1,6):
+#     if x % 2== 1:
+#         print(x, '*')
+#     else: 
+#         print(x,'**')
+
+# for x in range(5):
+#     for y in range(1, 6):
+#         print(y,end=" ")
+#     print()
+
+# for x in range(5):
+#     for y in range(5,0,-1):
+#         print(y,end=' ')
+#     print()
+
+# for x in range(5,0,-1):
+#     for y in range(5):
+#         print(x,end=' ')
+#     print()
+
+# for x in range(1,6):
+#     for y in range(5):
+#         print(x,end=' ')
+#     print() 
+
+# for x in range(5):
+#     for y in range(5):
+#         print('*',end=' ')
+#         if x==y:
+#             break           
+#     print()
+
+# for x in range(1,6):
+#     for y in range(1,6):
+#         print(y,end=' ')
+#         if x==y:
+#             break
+#     print()
+
+for x in range(1,6):
+    for y in range(1, x+1):
+        print(y,end=' ')
+    print()
